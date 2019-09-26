6a1d95c9215f200a53123cc16ad11123a7036138
egfrank <egfrank@umd.edu>
2019-09-26 19:10:08 -0400

    refactor layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,60 +4,12 @@ import styled from 'styled-components'
 
 import Img from "gatsby-image"
 
-import { Reset } from 'styled-reset'
-
-import Bio from "../components/bio"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
-
-import NavHeading from "../components/navheading"
-import CustomFooter from "../components/customfooter"
-import SocialLinks from "../components/sociallinks"
-import StyledBackgroundSection from "../components/styledbackgroundsection"
-
-import '../styles/index.css'
-
-const Div = styled.div`
-`
-const Grid = styled.div`
-  display: grid;
-  background: #FBCEB1;
-  grid-gap: 50px 100px;
-  grid-template-columns: 314px auto;
-  height: 100vh;
-`
-
-const Aside = styled.div`
-  background: #2F6853;
-  grid-column: 1 / 2;
-  border-right: 3px solid white;
-`
-
-const AsideContents = styled.div`
-  margin: 20px;
-  display: flex;
-  flex-direction: column;
-  height: 500px;
-  justify-content: space-between;
-`
-
-const PageTitle = styled.h1`
-  font-family: 'Karla', sans-serif;
-  color: #2F6853;
-  font-size: 36px;
-  float: right;
-  margin-top: 20px;
-  margin-right: 50px;
-`
-const Main = styled.div`
-  grid-column: 2 / 2;
-  overflow: auto;
-`
+import Layout from '../components/layout'
 
 const MainInside = styled.div`
   border: 2px solid white;
   box-size: border-box;
-  margin: 100px 50px 0 0;
+  margin: 50px 50px 0 0;
 `
 const MainText = styled.div`
   color: black;
@@ -69,47 +21,17 @@ const MainText = styled.div`
 const Em = styled.span`
   font-weight: bold;
 ` 
-const Navs = styled.div` 
-`
-const ImgContainer = styled.div`
-  margin: 0 auto;
-  width: 400px;
-`
+
+
 
 
 class HomePage extends React.Component {
 
   render() {
-    const { data } = this.props
+    
 
     return (
-      <Div>
-
-        <Reset />
-
-        <header>
-          <SEO title="Home" />
-        </header>
-
-        <Grid>
-
-          <Aside>
-            <AsideContents>
-              <Bio />
-
-              <Navs>
-                <NavHeading link='' title='About Me' style={{marginBottom: 24}}/>
-                <NavHeading link='/web' title='Web Development' style={{marginBottom: 24}}/>
-                <NavHeading link='/writing' title='Writing' />
-              </Navs>
-
-              <SocialLinks />
-            </AsideContents>
-
-           </Aside>
-           
-           <Main>
-              <PageTitle>About me</PageTitle>
+      <Layout title='About me'>
               <MainInside>
               <MainText>Hi I'm <Em>Elliot Frank</Em>, a web developer and writer based in Chicago.<br/><br/>
               As a web developer I mostly use React and Redux on the frontend and Django on the backend.
@@ -118,26 +40,11 @@ class HomePage extends React.Component {
               Email is the best way to get in touch.<br/><br/>
               </MainText>
               </MainInside>
-           </Main>
-
-
-         </Grid>
-
-    </Div>
+      </Layout>
     )
   }
 }
 
-// const query = graphql`
-//   query {
-//     file(relativePath: { eq: "me.jpg" }) {
-//       childImageSharp {
-//         fluid {
-//           ...GatsbyImageSharpFluid
-//         }
-//       }
-//     }
-//   }`
 
 export default HomePage
 
--- src/pages/web.js
+++ src/pages/web.js
@@ -1,116 +1,99 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import styled from 'styled-components'
 
-import { Reset } from 'styled-reset'
+import Img from "gatsby-image"
 
-import Bio from "../components/bio"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
-
-import NavHeading from "../components/navheading"
-import CustomFooter from "../components/customfooter"
-import SocialLinks from "../components/sociallinks"
-import StyledBackgroundSection from "../components/styledbackgroundsection"
+import Layout from '../components/layout'
 
 
-const Div = styled.div`
-`
-const Grid = styled.div`
-  display: grid;
-  background: #FBCEB1;
-  grid-gap: 50px 100px;
-  grid-template-columns: 314px auto;
-  height: 100vh;
+
+const PortfolioContainer = styled.div`
+  box-size: border-box;
+  margin: 50px 50px 0 0;
 `
 
-
-const Aside = styled.div`
-  background: #2F6853;
-  grid-column: 1 / 2;
+const PorfolioFlexBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: -30px 0;
 `
 
-const AsideContents = styled.div`
-  margin: 10px 0 0 10px;
+const ProjectItem = styled.div`
+  margin: 30px 0;
+  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2), 0 3px 20px 0 rgba(0, 0, 0, 0.19);
+  background-color: white;
 `
 
-const Main = styled.div`
-  grid-column: 2 / 2;
+const ProjectHeader = styled.div`
+  width: auto;
+  height: 350px;
+  background-color: grey;
 `
 
-const SubHeading = styled.h2`
+const ProjectName = styled.h2`
+  font-family: 'Source Sans Pro', serif;
+  font-weight: bold;
+  font-size: 26px;
+  margin: 26px 0;
+`
+
+const ProjectDescription = styled.p`
+  font-family: 'Source Serif Pro', serif;
+`
+
+const TextContainer = styled.div`
+  color: black;
   font-size: 20px;
-  color: white;
-  font-family: 'Source Sans Pro', sans-serif;
-  font-weight: normal;
+  line-height: 150%;
+  margin: 20px 20px;
+
 `
+const Em = styled.span`
+  font-weight: bold;
+` 
 
 
 
+const Project = () => {
 
+  return (
+    <ProjectItem>
+      <ProjectHeader>
+      </ProjectHeader>
 
-class HomePage extends React.Component {
+      <TextContainer>
+          <ProjectName>Bike Philadelphia</ProjectName>
+          <ProjectDescription>A web app that visualizes data about the 117 stations of Philadelphia’s bikeshare system (Indego), and is designed for users looking for either available bikes or empty spots in stations across the city. 
+          </ProjectDescription>
+      </TextContainer>
+    </ProjectItem>
+    );
+}
+
+class WebPortfolio extends React.Component {
+
   render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
-
+    
     return (
-      <Div>
+      <Layout title='Web development'>
+              <PortfolioContainer>
+              <PorfolioFlexBox>
+              <Project />
+              <Project />
+              <Project />
+              </PorfolioFlexBox>
+              </PortfolioContainer>
 
-        <Reset />
-
-        <header>
-          <SEO title="Home" />
-        </header>
-
-        <Grid>
-
-          <Aside>
-            <AsideContents>
-              <Bio />
-              <NavHeading link='' title='about me' />
-              <NavHeading link='/web-dev' title='web dev' />
-              <NavHeading link='/writing' title='writing' />
-            </AsideContents>
-
-           </Aside>
-           
-           <Main>
-              <p> web-dev</p>
-           </Main>
-
-
-         </Grid>
-
-    </Div>
+      </Layout>
     )
   }
 }
 
-export default HomePage
 
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
-      }
-    }
-  }
-`
+export default WebPortfolio
+
+
+
+
+
